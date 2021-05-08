639dd06b0d4c779f1d35a95e406ac14939fc9da5
Quinn <quinn@indra.ai>
2021-05-07 21:42:34 -0700

    the wall

---
+++ index.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+// COPYRIGHT (C)2021 QUINN MICHAELS. ALL RIGHTS RESERVED.
+
+// We are building a stdin wall tool
+const fs = require('fs');
+const path = require('path');
+
+const readline = require('readline');
+const {exec} = require('child_process');
+const { username } = require("os").userInfo()
+
+const shell = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+shell.setPrompt(`#${username} > `);
+shell.prompt();
+
+shell.on('line', question => {
+  exec(`wall ${question}`, (error, stdout, stderr) => {
+    console.log(`#${username} > ${stdout}`);
+  });
+}).on('pause', () => {
+}).on('resume', () => {
+}).on('close', () => {
+  console.log('EXIT');
+  process.exit(0);
+}).on('SIGCONT', () => {
+}).on('SIGINT', data => {
+  shell.close();
+}).on('SIGSTOP', () => {});
